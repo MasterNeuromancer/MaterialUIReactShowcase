925ce7262f43173f1db3fb82bf474de6b51a301f
Neuromancer93 <itrejonumber1@gmail.com>
2020-10-18 12:21:15 -0700

    material UI buttton, buttongroup and checkbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Checkbox from '@material-ui/core/Checkbox';
 import './App.css';
+
+function CheckboxExample () {
+  const [checked, setChecked] = React.useState(true);
+  return (
+    <div>
+      <Checkbox
+        checked={checked}
+        onChange={(e) => setChecked(e.target.checked)}
+        inputProps={{
+          'aria-label': 'secondary checkbox'
+        }}
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CheckboxExample />
+        <ButtonGroup variant='contained' color='primary'>
+          <Button
+            startIcon={<SaveIcon/>}>
+            Save
+          </Button>
+          <Button
+            startIcon={<DeleteIcon/>}>
+            Discard
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
